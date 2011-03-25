18ff6458d53dd7aa52237a2aa813b865c0ddf0b2
István Gazsi <theag3nt@gmail.com>
2011-03-25 17:33:55 +0100

    Massive code reorganization, improved error handling, more readable output.

--- zh-results.py
+++ zh-results.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 #
-# ZH eredmenyellenorzo - Copyright 2010 Istvan Gazsi
+# ZH eredmenyellenorzo - Copyright 2011 Istvan Gazsi
 #
 # -----
 #
-# Tetszolegesen megvaltoztathato a varakozasi ido, a linkek listaja es a ZH neve igy mas ZH-nal is hasznalhato.
+# Tetszolegesen megvaltoztathato a varakozasi ido, a linkek listaja es a ZH neve
+# igy mas ZH-nal is hasznalhato.
 # Ekezetes betuk hasznalata hibat okoz, egyelore...
 #
-# Javaslatokkal es hibajelentesekkel ezen a cimen kereshettek: < theag3nt at gmail dot com >
+# Javaslatokkal es hibajelentesekkel ezen a cimen kereshettek:
+# < theag3nt at gmail dot com >
 #
 # -----
 #
@@ -25,61 +27,138 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys, urllib2, hashlib, time#, optparse, base64
+import sys, numbers, urllib2, hashlib, datetime, time#, optparse, base64
 
 
 # ----- BEALLITASOK -----
-# (Hibas vagy nem megadott ertekek eseten a program futas kozben keri be az adatokat)
+# (Hibas vagy nem megadott ertekek eseten a program futas kozben keri be az
+#  adatokat)
 #
-# Varakozasi ido ket frissites kozott masodpercben. Ne legyen tul alacsony, mert akkor a gyakori frissitesek miatt tilthatjak a hozzaferest.
+# Varakozasi ido ket frissites kozott masodpercben. Ne legyen tul alacsony, mert
+# akkor a gyakori frissitesek miatt tilthatjak a hozzaferest. 0 eseten a program
+# megkerdezi indulaskor.
 sleep = 0
-sleep = int(sleep)
 # A figyelt ZH neve (ha tobb peldany is fut a programbol akkor hasznos).
-name = ""
+name = "" # Nincs hasznalva!
 # Az eredmenyeket tartalmazo fajlok elnevezese es URL-je.
 results = (
     # ("Pelda fajl", "http://pelda.bme.hu/~nev/eredmenyek.pdf"),
     )
-#hashes = []
+# Utolso frissites idejenek formatuma
+datimeformat = "%Y.%m.%d - %H:%M:%S"
+# 
 #
 # ----- BEALLITASOK VEGE -----
 
 
+class Result:
+    
+    def __init__(self, url, title=None, hash=None, info=""):
+        self.url = url
+        self.title = title
+        self.info = info
+        self.status = 0
+        self.statusinfo = ""
+        self.hash = hash
+
+    def setinfo(self, info=None):
+        if info:
+            self.info = info
+    
+    def check(self):
+        try:
+            file = urllib2.urlopen(self.url)
+            md5 = hashlib.md5()
+            md5.update(file.read())
+            if self.hash:
+                if (md5.hexdigest() == self.hash):
+                    self.status = 1
+                else:
+                    self.status = 2
+            else:
+                self.hash = md5.hexdigest()
+                self.status = 1
+            file.close()
+            statusinfo = ""
+        except urllib2.HTTPError, e:
+            if e.code == 404:
+                self.status = -1
+                self.hash = "HTTP404"
+            else:
+                self.status = -2
+                self.statusinfo = str(e.code)
+                self.hash = "HTTP{0}".format(e.code)
+        except urllib2.URLError, e:
+            self.status = -3
+            self.statusinfo = str(e.reason)
+            self.hash = "URLError"
+
+
+class ResultSet:
+    
+    def __init__(self):
+        self.results = []
+
+    def __len__(self):
+        return len(self.results)
+
+    def __getitem__(self, key):
+        if isinstance(key, numbers.Integral):
+            if key < len(self.results) and key >= 0:
+                return self.results[key]
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def append(self, result=None):
+        if isinstance(result, Result):
+            self.results.append(result)
+    
+    def populate(self, results=None):
+        for res in results:
+            if len(i) > 2:
+                self.results.append(Result(res[1], res[0], res[2]))
+            elif len(i) > 1:
+                self.results.append(Result(res[1], res[0]))
+
+
 class zh_results:
     
-    def __init__(self, name=None, results=[], hashes=[], sleep=600):
-        self.initialize(name, results, hashes, sleep)
-
-    def initialize(self, name=None, results=[], hashes=[], sleep=600):
-        self.name = name
-        self.results = tuple(results)
-        self.hashes = hashes
+    statusset = {-3:"Hiba", -2:"HTTP{0}", -1:"HTTP404", 0:"Ismeretlen", 1:"Valtozatlan", 2:"Frissitve!"}
+    infoset = {-3:"{0} - {1}", -2:"{0} - {1} szamu HTTP hiba", -1:"{0} - Nem talalhato", 0:"{0} - Nincs ellenorizve", 1:"{0} - Ellenorizve"}
+    datetimeformat = "%H:%M:%S"
+    
+    def __init__(self, name=None, results=[], sleep=600):
+        #self.name = name
+        self.results = ResultSet()
+        self.results.populate(tuple(results))
         self.sleep = sleep
-        self.maxlen = 0
-        for title, url in self.results:
-            if len(title) > self.maxlen:
-                self.maxlen = len(title)
+        self.maxlen = 3
+        self.maxstatuslen = 7
+        for status in self.statusset.values():
+            if len(status) > self.maxstatuslen:
+                self.maxstatuslen = len(status)
 
     def interactive_input(self):
         try:
             results = []
             self.print_header("Adatok bekerese...", False, False, False)
-            name = raw_input("ZH neve: ")
-            sleep = int(raw_input("Frissitesi gyakorisag (masodpercben): "))
-            if not sleep > 0:
-                raise ValueError
+            #while not self.name:
+            #    self.name = raw_input("ZH neve: ")
+            while self.sleep <= 0:
+                self.sleep = int(raw_input("Frissitesi gyakorisag (masodpercben): "))
             print "\nEllenorizendo eredmenyek (Ctrl+C-vel befejezheti a hozzaadast):"
             while True:
-                results.append((raw_input("\nCimke: "), raw_input("URL (webcim): ")))
+                self.results.append(Result(raw_input("\nURL (webcim): "), raw_input("Nev: ")))
         except KeyboardInterrupt:
-            if results:
-                self.initialize(name, results, sleep=sleep)
+            if self.results:
                 return True
             else:
-                print "Hiba: Nem adott meg ellenorizendo eredmenyeket"
+                print "\nHiba: Nem adott meg ellenorizendo eredmenyeket"
                 return False
         except ValueError, e:
-            print "Hiba: Ervenytelen frissitesi gyakorisag"
+            print "\nHiba: Ervenytelen frissitesi gyakorisag"
             return False
 
     def format_sleep(self):
@@ -97,6 +176,12 @@ class zh_results:
             return "{0} masodpercenkent".format(self.sleep)
         else:
             return None
+    
+    def format_time(self):
+        if self.datetimeformat:
+            return datetime.datetime.now().strftime(self.datetimeformat)
+        else:
+            return datetime.datetime.now().strftime("%Y.%m.%d. %H:%M:%S")
 
     def print_header(self, status=None, show_sleep=True, show_name=False, can_quit=True):
         print 100 * "\n"
@@ -109,68 +194,57 @@ class zh_results:
             print "Kilepes a Ctrl+C billentyukombinacioval...\n"
         else:
             print "Befejezes a Ctrl+C billentyukombinacioval...\n"
-        if show_name:
-            print self.name, "eredmenyei\n"
+        #if show_name:
+        #    print self.name, "eredmenyei\n"
 
-    def print_results(self, statuses=[]):
-        if len(statuses) == len(self.results):
+    def print_results(self):
+        if len(self.results):
             self.print_header(show_name=True)
-            for i in xrange(len(statuses)):
-                print "{0:{1}} -".format(self.results[i][0], self.maxlen),
-                if not statuses[i]:
-                    print "Valtozatlan"
-                else:
-                    print " FRISSITVE! <-"
+            print "{0:{width}} | {1:{statuswidth}} | {2}".format("Nev", "Statusz", "Info", width=self.maxlen, statuswidth=self.maxstatuslen)
+            print "{0:-<{width}} | {1:-<{statuswidth}} | {2}".format("", "", "--------------------", width=self.maxlen, statuswidth=self.maxstatuslen)
+            for res in self.results:
+                print "{0:{width}} | {1:{statuswidth}} | {2}".format(res.title, self.statusset[res.status].format(res.statusinfo), res.info, width=self.maxlen, statuswidth=self.maxstatuslen)
 
     def check(self, interactive=False):
         try:
-            if not (name and results and sleep > 0):
+            #if not (self.name and self.results and self.sleep > 0):
+            if not (self.results and self.sleep > 0):
                 if not self.interactive_input():
                     return False
             if self.sleep > 0:
                 if self.results:
-                    statuses = []
+                    first_run = True
                     while True:
-                        if len(self.hashes) == 0:
+                        if first_run:
                             self.print_header("Kezdeti allapotok tarolasa...")
-                            for title, url in self.results:
-                                file = urllib2.urlopen(url)
-                                md5 = hashlib.md5()
-                                md5.update(file.read())
-                                self.hashes.append(md5.hexdigest())
-                                statuses.append(False)
-                        elif len(self.hashes) == len(self.results):
-                            self.print_header("Eredmenyek ellenorzese...")
-                            for i in xrange(len(self.hashes)):
-                                file = urllib2.urlopen(self.results[i][1])
-                                md5 = hashlib.md5()
-                                md5.update(file.read())
-                                if (md5.hexdigest() == self.hashes[i]):
-                                    statuses[i] = False
-                                else:
-                                    statuses[i] = True
-                        self.print_results(statuses)
+                            first_run = False
+                            for res in self.results:
+                                if len(res.title) > self.maxlen:
+                                    self.maxlen = len(res.title)
+                                res.check()
+                        self.print_header("Eredmenyek ellenorzese...")
+                        for res in self.results:
+                            res.check()
+                            res.setinfo(self.infoset[res.status].format(self.format_time(), res.statusinfo))
+                        self.print_results()
                         time.sleep(self.sleep)
                 else:
-                    print "Hiba: Nincsenek ellenorizendo eredmenyek"
+                    print "\nHiba: Nincsenek ellenorizendo eredmenyek"
                     return False
             else:
-                print "Hiba: Nincs megadva ervenyes frissitesi gyakorisag"
+                print "\nHiba: Nincs megadva ervenyes frissitesi gyakorisag"
                 return False
         except KeyboardInterrupt:
             print "\n\nKilepes..."
             time.sleep(1)
             return True
-        except urllib2.URLError, e:
-            print "Hiba:", str(e.reason)
-            return False
         except Exception, e:
             print "Ismeretlen hiba:", str(e)
             return False
 
 
 if __name__ == "__main__":
-    result = zh_results(name, results, sleep=sleep)
+    result = zh_results(name, results, sleep)
     if not result.check():
         raw_input("Nyomjon <Enter>-t a kilepeshez...")
         sys.exit(1)
